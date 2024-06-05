dd2f68aebe025c101189b595cfd95f1b6cb2be17
Thaddeus Rosales <thadyboygwapo82@gmail.com>
2024-06-05 11:19:33 +0800

    Initial commit

---
+++ src/main/kotlin/dev/tadyboii/NumberToText/NumberToTextApplication.kt
@@ -0,0 +1,11 @@
+package dev.tadyboii.NumberToText
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class NumberToTextApplication
+
+fun main(args: Array<String>) {
+	runApplication<NumberToTextApplication>(*args)
+}
---
+++ src/test/kotlin/dev/tadyboii/NumberToText/NumberToTextApplicationTests.kt
@@ -0,0 +1,13 @@
+package dev.tadyboii.NumberToText
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class NumberToTextApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
