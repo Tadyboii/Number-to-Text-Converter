e8f70370107a93865723f539dc0919ede9f5ae37
Thaddeus Rosales <thadyboygwapo82@gmail.com>
2024-06-06 13:04:15 +0800

    improve code

--- src/main/kotlin/dev/tadyboii/NumberToText/util/NumberToTextConverter.kt
+++ src/main/kotlin/dev/tadyboii/NumberToText/util/NumberToTextConverter.kt
@@ -2,73 +2,73 @@ package dev.tadyboii.NumberToText.util
 
 class NumberToTextConverter {
 
+    private fun divideDigitsByThree(number: Int): List<List<Int>> {
+        val reversedDigits = number.toString().reversed().map { it.toString().toInt() }
+        return reversedDigits.chunked(3)
+    }
+
     fun convert(number: Int): String {
 
-        val digits = number.toString().reversed().map { it.toString().toInt() }.toIntArray()
-        var text: String = ""
+        val periods = divideDigitsByThree(number)
+        var finalText: String = ""
+        val prefixes: Array<String> = arrayOf("", "thousand", "million", "billion")
 
-        digits.forEachIndexed { index, digit ->
-            if (index == 0 || index == 3) {
-                text = text.takeIf { index != 3 } ?: "thousand $text"
-                text = when (digit) {
-                    0 -> "zero $text"
-                    1 -> "one $text"
-                    2 -> "two $text"
-                    3 -> "three $text"
-                    4 -> "four $text"
-                    5 -> "five $text"
-                    6 -> "six $text"
-                    7 -> "seven $text"
-                    8 -> "eight $text"
-                    9 -> "nine $text"
-                    else -> text
-                }
-            } else if (index == 1){
-                if(digit == 1) {
-                    text = when (text) {
-                        "zero" -> "ten"
-                        "one" -> "eleven"
-                        "two" -> "twelve"
-                        "three" -> "thirteen"
-                        "four" -> "fourteen"
-                        "five" -> "fifteen"
-                        "six" -> "sixteen"
-                        "seven" -> "seventeen"
-                        "eight" -> "eighteen"
-                        "nine" -> "nineteen"
-                        else -> text
+        periods.forEachIndexed { periodIndex, periodDigits ->
+            var periodText: String = ""
+            var firstDigitOfPeriod: Int = 0
+
+            periodDigits.forEachIndexed { digitIndex, digit ->
+                if (digitIndex == 0 || digitIndex == 2) {
+                    firstDigitOfPeriod = digit
+                    var hundredPrefix: String? = ""
+                    hundredPrefix = hundredPrefix.takeIf {digitIndex != 2 } ?: "hundred"
+                    periodText = when (digit) {
+                        0 -> "zero"
+                        1 -> "one $hundredPrefix $periodText"
+                        2 -> "two $hundredPrefix $periodText"
+                        3 -> "three $hundredPrefix $periodText"
+                        4 -> "four $hundredPrefix $periodText"
+                        5 -> "five $hundredPrefix $periodText"
+                        6 -> "six $hundredPrefix $periodText"
+                        7 -> "seven $hundredPrefix $periodText"
+                        8 -> "eight $hundredPrefix $periodText"
+                        9 -> "nine $hundredPrefix $periodText"
+                        else -> periodText
                     }
-                }else{
-                    text = text.takeIf { it != "zero" } ?: ""
-                    text = when (digit) {
-                        2 -> "twenty $text"
-                        3 -> "thirty $text"
-                        4 -> "forty $text"
-                        5 -> "fifty $text"
-                        6 -> "sixty $text"
-                        7 -> "seventy $text"
-                        8 -> "eighty $text"
-                        9 -> "ninety $text"
-                        else -> text
+                } else if (digitIndex == 1) {
+                    if (digit == 1) {
+                        periodText = when (firstDigitOfPeriod) {
+                            0 -> "ten"
+                            1 -> "eleven"
+                            2 -> "twelve"
+                            3 -> "thirteen"
+                            4 -> "fourteen"
+                            5 -> "fifteen"
+                            6 -> "sixteen"
+                            7 -> "seventeen"
+                            8 -> "eighteen"
+                            9 -> "nineteen"
+                            else -> periodText
+                        }
+                    } else {
+                        periodText = periodText.takeIf { firstDigitOfPeriod != 0 } ?: ""
+                        periodText = when (digit) {
+                            2 -> "twenty $periodText"
+                            3 -> "thirty $periodText"
+                            4 -> "forty $periodText"
+                            5 -> "fifty $periodText"
+                            6 -> "sixty $periodText"
+                            7 -> "seventy $periodText"
+                            8 -> "eighty $periodText"
+                            9 -> "ninety $periodText"
+                            else -> periodText
+                        }
                     }
-                }
-            }else if (index == 2){
-                text = text.takeIf { it != "zero" } ?: ""
-                text = when (digit) {
-                    1 -> "one hundred $text"
-                    2 -> "two hundred $text"
-                    3 -> "three hundred $text"
-                    4 -> "four hundred $text"
-                    5 -> "five hundred $text"
-                    6 -> "six hundred $text"
-                    7 -> "seven hundred $text"
-                    8 -> "eight hundred $text"
-                    9 -> "nine hundred $text"
-                    else -> text
                 }
             }
-        }
 
-        return text
+            finalText = "$periodText ${prefixes[periodIndex]} $finalText".trimEnd()
+        }
+        return finalText
     }
 }
